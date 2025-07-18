35b74b345391a9672599a3f5db571debf659a186
arya satwika <aryastwk95@gmail.com>
2025-07-18 22:40:14 +0700

    updated schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,27 +1,23 @@
-import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { integer, pgTable, serial, text } from 'drizzle-orm/pg-core';
 
-export const usersTable = pgTable('users_table', {
+export const usersTable = pgTable('users', {
   id: serial('id').primaryKey(),
   name: text('name').notNull(),
-  age: integer('age').notNull(),
-  email: text('email').notNull().unique(),
 });
 
-export const postsTable = pgTable('posts_table', {
+
+export const routineTable = pgTable('routine', {
   id: serial('id').primaryKey(),
   title: text('title').notNull(),
-  content: text('content').notNull(),
+  startAt: text('start_at').notNull(),
   userId: integer('user_id')
     .notNull()
     .references(() => usersTable.id, { onDelete: 'cascade' }),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at')
-    .notNull()
-    .$onUpdate(() => new Date()),
 });
 
-export type InsertUser = typeof usersTable.$inferInsert;
-export type SelectUser = typeof usersTable.$inferSelect;
-
-export type InsertPost = typeof postsTable.$inferInsert;
-export type SelectPost = typeof postsTable.$inferSelect;
+export const checkedBlocks = pgTable('checked_blocks', {
+  routineId: integer('routine_id')
+    .notNull()
+    .references(() => routineTable.id, { onDelete: 'cascade' }),
+  date: text('date').notNull(),
+});
