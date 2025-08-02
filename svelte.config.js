f60e33e9697b4333764b5c8ab03f3646d99bc34a
arya satwika <aryastwk95@gmail.com>
2025-08-02 14:30:13 +0700

    change  cconfig svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,15 @@
 import adapter from '@sveltejs/adapter-netlify';
 
-export default {
-  kit: {
-    adapter: adapter()
-  }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    kit: {
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: '#svelte',
+
+        // Svelte requires an adapter for deployment
+        adapter: adapter()
+    }
 };
+
+export default config;
+
